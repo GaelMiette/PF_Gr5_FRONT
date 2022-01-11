63e4185a3f3aa58a352bd0c0f60d0aee61e3eb1f
Gael <gael.miette@hotmail.fr>
2022-01-11 22:45:35 +0100

    troisième commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
