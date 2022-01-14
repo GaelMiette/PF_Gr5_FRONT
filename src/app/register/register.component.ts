3d9b05d736441fc2bb4dd14ef02fc12c40a7177c
Gael <gael.miette@hotmail.fr>
2022-01-14 10:20:48 +0100

    modif comp register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,6 +11,9 @@ export class RegisterComponent implements OnInit {
 
   espace : boolean=true;
   message;
+  departements:any;
+  isRecruiter;
+  BASE_URL: "http://localhost:8080/danavalley/api/candidats";
 
   candidate: any = {
     name : "",
@@ -40,6 +43,14 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
    // document.getElementById("esp_rec").style.display='none';
     document.getElementById("form_rec").style.visibility='hidden';
+
+    // récupération de la liste des départements dispo depuis la base
+    // dynamiser les boutons radio du html avec ?
+    this.http.get(this.BASE_URL + "/departements").subscribe(
+      response=>{
+        this.departements = response;
+      }
+    )
   }
 
   toggle(){
@@ -58,12 +69,52 @@ export class RegisterComponent implements OnInit {
   }
 }
 
+find_departement(user:any){
+  // On récupère depuis le formulaire l'id d'un département.
+  // Pour l'enregistrement en db, on a besoin du département complet.
+  // Cette fonction assigne au user le département correspondant à l'id du formulaire.
+  let departement = null;
+  
+  this.departements.map(dept => {
+    if(dept.id == user.departement_id){
+      departement = dept;
+    }
+  });
+  
+  user.departement = departement;
+
+  return user;
+}
+
+send_to_db(endpoint, user){
+  // création d'un nouveau user (candidat ou recruteur) en db et auto-login
+  this.http.post(
+
+    this.BASE_URL + endpoint,
+    JSON.stringify(user),
+    {headers: new HttpHeaders({"Content-Type": "application/json"})}
+
+  ).subscribe(
+
+    response =>{
+      let toStore = JSON.stringify(user);
+      sessionStorage.setItem("user", toStore);
+      this.router.navigate(["/home"]);
+    },
+
+    error =>{
+      alert("bouh")
+    }
+    
+  )
+  
+}
 
 register_candidate(){
 
   const body = JSON.stringify(this.candidate);
 
-    this.http.post("http://localhost:8080/danavalley/api/candidats", body, {
+    this.http.post(this.BASE_URL, body, {
       headers: new HttpHeaders({
         "Content-Type": "application/json"
       })
@@ -78,24 +129,27 @@ register_candidate(){
         });
 }
 
-
-  register(isRecruiter: boolean){
-
-    let user = this.candidate;
-
-    if(isRecruiter){
-      user = this.recruiter;
-    }
-
-    // vérification bdd
-    let isRegistered: boolean = true;
+registerRecruiter(){
     
-    if(isRegistered){
-      delete user.pwd;
-      let toStore = JSON.stringify(user);
-      sessionStorage.setItem("user", toStore);
-      this.router.navigate(["/accueil"]);
+  let user = this.find_departement(this.recruiter);
+  console.log("recruiter : ", user);
+
+  // vérification bdd si le mail existe déjà
+  this.http.get<Recruiter>(this.BASE_URL + "/recruteursmail/" + user.mail).subscribe(
+    
+    async response => {
+      if(response == null){
+        // si la réponse vaut null, c'est qu'aucun recruteur avec ce mail n'existe, on le créé
+        this.send_to_db("/recruteurs", user);
+      }
     }
-  }
+
+  )
+}
+
+register(isRecruiter: boolean){
+
+  isRecruiter ? this.registerRecruiter() : this.register_candidate();
+}
 
 }
