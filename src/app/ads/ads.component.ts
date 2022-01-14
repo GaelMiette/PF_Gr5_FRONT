60fd6b9e1cd6233fe08e1c409583bb08d8c1a779
Elise Coudurier <eliz.cou@gmail.com>
2022-01-14 17:28:44 +0100

    liste annonce et description annonce dynamique

--- src/app/ads/ads.component.ts
+++ src/app/ads/ads.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-ads',
@@ -8,29 +10,57 @@ import { Component, OnInit } from '@angular/core';
 export class AdsComponent implements OnInit {
   
 
+  // Mylist : any;
+  // date : string;
+  // constructor() { }
+
+  // ngOnInit(): void {
+
+  //   this.date = new Date(Date.now()).toDateString();
+
+  //   this.Mylist=[{
+  //     titre: "Location de menhirs",
+  //     description: "Je souhaite trouver quelqu'un pour louer mon menhir.",
+  //     categorie:"petits services",
+  //     date: "12.01.2022",
+  //     salaire : "300€",
+  //     type_contrat : "ponctuel",
+  //     dept : {
+  //       id : 22,
+  //       name : "Côtes d'Armor"
+  //     },
+  //     recruteur: {
+  //       name: "Obelix"
+  //     }
+  //   }]
+  // }
+
+
   Mylist : any;
   date : string;
-  constructor() { }
+  constructor(private http : HttpClient, private route : Router) { }
 
   ngOnInit(): void {
 
     this.date = new Date(Date.now()).toDateString();
 
-    this.Mylist=[{
-      titre: "Location de menhirs",
-      description: "Je souhaite trouver quelqu'un pour louer mon menhir.",
-      categorie:"petits services",
-      date: "12.01.2022",
-      salaire : "300€",
-      type_contrat : "ponctuel",
-      dept : {
-        id : 22,
-        name : "Côtes d'Armor"
+    this.init();
+
+  }
+
+  init(){
+    this.http.get("http://localhost:8080/danavalley/api/annonces").subscribe(
+      response => {
+        this.Mylist = response;
       },
-      recruteur: {
-        name: "Obelix"
+      err=>{
+        console.log("**********KO")
       }
-    }]
+    )
+  }
+
+  seeDescription(id){
+    this.route.navigate(['/ad/'+id]);
   }
 
 }
--- src/app/little-ad/little-ad.component.ts
+++ src/app/little-ad/little-ad.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-little-ad',
@@ -7,29 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LittleAdComponent implements OnInit {
 
+  // annonce : any;
+  // constructor() { }
+
+  // ngOnInit(): void {
+  //   this.annonce={
+  //     titre: "Location de menhirs",
+  //     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //     categorie:"petits services",
+  //     date: "12.01.2022",
+  //     salaire : "300€",
+  //     type_contrat : "ponctuel",
+  //     dept : {
+  //       id : 22,
+  //       name : "Côtes d'Armor"
+  //     },
+  //     recruteur : {
+  //       name : "Obelix"
+  //     }
+  //   }
+  // }
+
+
   annonce : any;
-  constructor() { }
+  id : any;
+  constructor(private http : HttpClient, private route : ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.annonce={
-      titre: "Location de menhirs",
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      categorie:"petits services",
-      date: "12.01.2022",
-      salaire : "300€",
-      type_contrat : "ponctuel",
-      dept : {
-        id : 22,
-        name : "Côtes d'Armor"
-      },
-      recruteur : {
-        name : "Obelix"
+    this.route.params.subscribe(params =>{
+      this.id = params['id'];
+    });
+
+    this.http.get("http://localhost:8080/danavalley/api/annonces/"+this.id).subscribe(
+      response =>{
+        this.annonce = response;
       }
-    }
+    )
   }
 
   candidater(){
     
   }
+
 
 }
