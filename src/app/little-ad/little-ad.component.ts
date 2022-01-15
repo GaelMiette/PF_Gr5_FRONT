f397068f9ce3d7305409c6e1a262d4f0935ba783
Gael <gael.miette@hotmail.fr>
2022-01-15 16:51:07 +0100

    ajout et dynamisation boutons modif/suppr côté recruteur

--- src/app/little-ad/little-ad.component.ts
+++ src/app/little-ad/little-ad.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
+import { asLiteral } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-little-ad',
@@ -11,9 +12,12 @@ export class LittleAdComponent implements OnInit {
 
   annonce : any;
   id : any;
-  constructor(private http : HttpClient, private route : ActivatedRoute) { }
+  user:any;
+  constructor(private http : HttpClient, private route : ActivatedRoute , private router : Router) { }
 
   ngOnInit(): void {
+
+    this.user=sessionStorage.getItem("user");
     this.route.params.subscribe(params =>{
       this.id = params['id'];
     });
@@ -27,6 +31,19 @@ export class LittleAdComponent implements OnInit {
 
   candidater(){
     
+  }
+
+  modifier(){
+    this.router.navigate(['/upd_ad/'+this.id]);
+  }
+
+  supprimer(){
+    this.http.delete(sessionStorage.getItem("BASE_URL")+"/annonces/"+this.id)
+    .subscribe((response)=>{
+      alert(response);
+      this.router.navigate(['/espc']);
+    })
+
   }
 
 
