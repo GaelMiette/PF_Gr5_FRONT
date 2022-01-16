0e3c6a0bf9b48326d30bf938d39166d346d26f6c
Elise Coudurier <eliz.cou@gmail.com>
2022-01-16 15:34:31 +0100

    debut postule candidat

--- src/app/little-ad/little-ad.component.ts
+++ src/app/little-ad/little-ad.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Anounce } from '../shared/anounce';
+import { Candidate } from '../shared/candidate';
 
 @Component({
   selector: 'app-little-ad',
@@ -12,22 +13,77 @@ export class LittleAdComponent implements OnInit {
 
   annonce : Anounce = new Anounce();
   id : any;
-  user:any;
+  user:Candidate = new Candidate();
+  isCandidat: boolean = false;
   constructor(private http : HttpClient, private route : ActivatedRoute , private router : Router) { }
   departements = JSON.parse(sessionStorage.getItem("departements"));
   
   async ngOnInit() {
+    
     this.user=JSON.parse(sessionStorage.getItem("user"));
+    console.log(this.user);
     this.route.params.subscribe(params =>{
       this.id = params['id'];
     });
 
     this.annonce=await this.http.get<Anounce>("http://localhost:8080/danavalley/api/annonces/"+this.id).toPromise()
     console.log(this.annonce)
+
+    this.checkPostule();
+    console.log(this.isCandidat)
   }
 
-  candidater(){
-    
+  checkPostule(){
+    for (let candidat of this.annonce.listeCandidats){
+      if(candidat.id==this.user.id){
+        this.isCandidat=true;
+        break;
+      }
+      else
+        this.isCandidat=false
+    }
+  }
+
+  async candidater(){
+    this.user.listeAnnonces.push(this.annonce);
+    const body = JSON.stringify(this.user);
+    // this.http.put(sessionStorage.getItem("BASE_URL")+"/candidats", body, {
+    //   headers: new HttpHeaders({
+    //     "Content-Type": "application/json"
+    //   })
+    // }).
+      // subscribe(async response => {
+
+      //   console.log("crud service post OK");
+      //   //recharge le user
+
+      //   let toStore = await this.http.get(sessionStorage.getItem("BASE_URL")+"/candidats/"+this.user.id);
+      //   sessionStorage.setItem("user", JSON.stringify(toStore));
+      //   this.user.isRecruter = false;
+      //   window.location.reload();
+      // },
+      // err => {
+      //   console.log("crud service post KO")
+      // });
+
+     await this.http.put(sessionStorage.getItem("BASE_URL")+"/candidats", body, {
+          headers: new HttpHeaders({
+            "Content-Type": "application/json"
+          })
+        }).toPromise();
+        
+      this.user = await this.http.get<Candidate>(sessionStorage.getItem("BASE_URL")+"/candidats/"+this.user.id
+      ).toPromise();
+      this.user.isRecruiter = false;
+      console.log(this.user);
+      sessionStorage.setItem("user", JSON.stringify(this.user));
+
+  }
+
+  
+
+  retCandidature(){
+
   }
 
   modifier(){
