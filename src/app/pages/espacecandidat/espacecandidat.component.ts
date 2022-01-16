33c6f5de31ad10b13cf862c0a4e8d9db1f3ff032
Dorian <guillotdorian@hotmail.fr>
2022-01-16 15:32:40 +0100

    espace candiat ok + modifs dans candidate, recruiter et register (anneesxp remplacé par anneesXP)

--- src/app/pages/espacecandidat/espacecandidat.component.ts
+++ src/app/pages/espacecandidat/espacecandidat.component.ts
@@ -1,94 +1,62 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Candidate } from 'src/app/shared/candidate';
 
 @Component({
-  selector: 'app-espacecandidat',
-  templateUrl: './espacecandidat.component.html',
-  styleUrls: ['./espacecandidat.component.css']
+	selector: 'app-espacecandidat',
+	templateUrl: './espacecandidat.component.html',
+	styleUrls: ['./espacecandidat.component.css']
 })
 export class EspacecandidatComponent implements OnInit {
 
-  shouldShow = false;
-  message;
+	shouldShow = false;
+	message;
+	user: Candidate = new Candidate();
+	BASE_URL = null;
+	departements = [];
 
-  user ={
-    id:1,
-    age:25,
-    nom:"trucmuche",
-    prenom:"toto",
-    departement_id:22,
-    anneesxp:0,
-    mail: "aze@aze",
-    mdp : "zer",
-    profession:"developpeur",
-    version:2,
-  }
+	constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
+	ngOnInit(): void {
+		this.user = JSON.parse(sessionStorage.getItem("user"));
+		this.BASE_URL = sessionStorage.getItem("BASE_URL");
+		this.departements = JSON.parse(sessionStorage.getItem("departements"));;
+	}
 
-  constructor(private http:HttpClient , private route: ActivatedRoute) { }
+	toggle_display() {
+		this.shouldShow = !this.shouldShow;
+	}
 
-  ngOnInit(): void {
-    //this.user = JSON.parse(sessionStorage.getItem("user"));
-    console.log(this.user);
-  }
+	find_departement(id){
+		// On récupère depuis le formulaire l'id d'un département.
+		// Pour l'enregistrement en db, on a besoin du département complet.
+		// Cette fonction récupère le département correspondant à l'id du formulaire.
+		let departement = null;
+		
+		this.departements.map(dept => {
+		  if(dept.id == id){
+			departement = dept;
+		  }
+		});
+	
+		return departement;
+	
+	  }
 
-  toggle_display(){
-    if(this.shouldShow==true){
-      this.display_form(false)
-    }else{
-      this.display_form(true)
-    }
-  }
+	async update() {
 
-  display_form(should){
-    this.shouldShow = should
-  }
+		this.shouldShow = false;
+		this.user.departement = this.find_departement(this.user.departement_id);
+		let body = JSON.stringify(this.user);
+		let headers = {headers: new HttpHeaders({'Content-Type': 'application/json'})}
+		
+		await this.http.put(this.BASE_URL + "/candidats", body, headers).toPromise();
 
-
-put_function(tmp){
-  this.http
-  .put(
-    sessionStorage.getItem('BASE_URL') + '/candidats',
-    JSON.stringify(tmp),
-    {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-    }
-  )
-  .subscribe(
-    (response) => {
-      this.message = 'Profil modifié';
-      sessionStorage.setItem("user", tmp);
-    },
-    (err) => {
-      this.message = 'no bueno la modificacion';
-    }
-  );
-}
-
-update(){
-  let tmp;
-  this.http.get(sessionStorage.getItem("BASE_URL")+"/candidatsmail/"+this.user.mail)
-  .subscribe(
-    (response)=>{
-      console.log(response.toString())
-      if(response!=null)
-      tmp=response;
-
-      if(tmp!=null){
-        tmp=this.user;
-        console.log("tmp prend la valeur du user");
-      }else{
-        this.message="user n'existe pas dans la db.";
-        return tmp;
-      }
-      this.put_function(tmp);
-      console.log("j'arrive au bout : "+tmp.nom +" "+tmp.prenom);
-      sessionStorage.setItem("user",tmp);
-    }
-  )
-        
-  }
+		this.user = await this.http.get<Candidate>(this.BASE_URL + "/candidats/"+ this.user.id).toPromise();
+		this.user.isRecruiter = false;
+		body = JSON.stringify(this.user);
+		sessionStorage.setItem("user", body);
+		alert("modification enregistéres");
+	}
 }
--- src/app/shared/candidate.ts
+++ src/app/shared/candidate.ts
@@ -1,13 +1,17 @@
+import { Department } from "./department";
+
 export class Candidate {
     id = 0;
     nom = "";
     prenom = "";
     profession = "";
-    anneesxp = 0;
+    anneesXP = 0;
     age = 0;
     departement_id = 22;
     mail = ""; 
     mdp = ""; 
     isRecruiter = false;
     version = 0;
+    departement:Department = new Department()
+    listeAnnonces = [];
 }
